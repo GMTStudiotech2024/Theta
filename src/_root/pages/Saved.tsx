281150278e8faf24efb2e1b2b2101fadcbacb3e2
GMTStudiotech2024 <alstonduck@gmail.com>
2024-05-14 08:35:00 +0800

    commit

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -19,13 +19,6 @@ const Saved = () => {
   return (
     <div className="saved-container">
       <div className="flex gap-2 w-full max-w-5xl">
-        <img
-          src="/assets/icons/save.svg"
-          width={36}
-          height={36}
-          alt="edit"
-          className="invert-white"
-        />
         <h2 className="h3-bold md:h2-bold text-left w-full text-fuchsia-400">Saved Posts 🗃️</h2>
       </div>
 
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -54,10 +54,10 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
           <h3 className="base-medium text-light-2 mb-2 mt-6">
             Drag photo here
           </h3>
-          <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG</p>
+          <p className="text-yellow-400 small-regular mb-6">SVG, PNG, JPG</p>
 
           <Button type="button" className="shad-button_dark_4">
-            Select from computer
+            Select from your device 
           </Button>
         </div>
       )}
