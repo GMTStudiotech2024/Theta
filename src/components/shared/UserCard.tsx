0f81c06ae943558091c231da7e1e96c21a3ce870
GMTStudiotech2024 <153701583+GMTStudiotech2024@users.noreply.github.com>
2024-05-20 08:14:03 +0800

    commit

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -13,19 +13,19 @@ const UserCard = ({ user }: UserCardProps) => {
       <img
         src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
         alt="creator"
-        className="rounded-full w-14 h-14"
+        className="rounded-full w-14 h-14 "
       />
 
       <div className="flex-center flex-col gap-1">
         <p className="base-medium text-light-1 text-center line-clamp-1">
           {user.name}
         </p>
-        <p className="small-regular text-amber-300 text-center line-clamp-1">
+        <p className="small-regular text-sky-50 text-center line-clamp-1">
           @{user.username}
         </p>
       </div>
 
-      <Button type="button" size="sm" className="shad-button_primary px-5">
+      <Button type="button" size="sm" className="shad-button_primary px-1.5">
         View User status
       </Button>
     </Link>
